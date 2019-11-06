09e25510cc2f0334bfe88a400911e7b423c2cd34
python77777121 <you@example.com>
2019-11-06 19:29:17 +0300

    heroku allouth git

--- mysite/githubus/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import include, path
-
-urlpatterns = [
-    path('', views.git, name = 'githubus'),
-    
-]
--- mysite/githubus/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def git(request):
-    return render(request, "githubus/githubus.html",{"Name": "Githubus"})
